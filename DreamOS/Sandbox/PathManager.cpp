cc618e00ca0753fe737d28dd8b9cc807b44776a8
Idan Beck <idanbeck@gmail.com>
2015-12-28 11:54:29 -0800

    PathManager now parsing dreampaths.txt off Dreamm Root environment variable

---
+++ DreamOS/Sandbox/PathManager.cpp
@@ -0,0 +1,93 @@
+#include "PathManager.h"
+
+PathManager::PathManager() :
+	m_pmapNVPPaths(NULL)
+{
+	m_cszPathValues_n = sizeof(m_cszPathValues) / sizeof(m_cszPathValues[0]);
+
+	m_pmapNVPPaths = new std::map<PATH_VALUE_TYPE, wchar_t*>();
+	ACNM(m_pmapNVPPaths, "Failed to allocate paths map");
+}
+
+PathManager::~PathManager() {
+	ACRM(Dealloc(), "PathManager failed to deallocate paths");
+
+	if (m_pmapNVPPaths != NULL) {
+		delete m_pmapNVPPaths;
+		m_pmapNVPPaths = NULL;
+	}
+}
+
+RESULT PathManager::Dealloc() {
+	return R_NOT_IMPLEMENTED;
+}
+
+RESULT PathManager::RegisterPath(wchar_t *pszName, wchar_t *pszValue) {
+	RESULT r = R_PASS;
+	std::pair<std::map<PATH_VALUE_TYPE, wchar_t*>::iterator, bool>retVal;
+	wchar_t *pszPath = NULL;
+
+	DEBUG_LINEOUT("Registering Name:%S Value:%S", pszName, pszValue);
+	PATH_VALUE_TYPE pathValueType = GetPathValueType(pszName);
+	CBM((pathValueType != PATH_INVALID), "Not a valid path value type");
+
+	retVal = m_pmapNVPPaths->insert(std::pair<PATH_VALUE_TYPE, wchar_t*>(pathValueType, pszValue));
+	CBM((retVal.second != false), "Failed to insert NVP into Paths map");
+
+	CRM(GetValuePath(pathValueType, pszPath), "Failed to get path value");
+	DEBUG_LINEOUT("Registered type:%d Value:%S", pathValueType, pszPath);
+
+Error:
+	if (pszPath != NULL) {
+		delete[] pszPath;
+		pszPath = NULL;
+	}
+
+	return r;
+}
+
+// Set up path manager
+RESULT PathManager::InitializePaths() {
+	DEBUG_LINEOUT("PathManager Initialize Paths");
+	return R_VIRTUAL;
+}
+
+PATH_VALUE_TYPE PathManager::GetPathValueType(wchar_t *pszValue) {
+	for (int i = 0; i < m_cszPathValues_n; i++)
+		if (wcscmp(m_cszPathValues[i], pszValue) == 0)
+			return (PATH_VALUE_TYPE)(i);
+
+	return PATH_INVALID;
+}
+
+RESULT PathManager::GetValuePath(PATH_VALUE_TYPE type, wchar_t* &n_pszPath) {
+	RESULT r = R_PASS;
+	int n_pszPath_n = 0;
+	wchar_t *pszDreamPath = NULL;
+	int pszDreamPath_n = 0;
+	wchar_t *pszValue = NULL;
+	int pszValue_n = 0;
+	errno_t err;
+
+	// Check to see we have the type registered 
+	std::map<PATH_VALUE_TYPE, wchar_t*>::iterator it = m_pmapNVPPaths->find(type);
+	CBM((it != m_pmapNVPPaths->end()), "Path Value %d not found in map", type);
+	
+	pszValue = m_pmapNVPPaths->at(type);
+	pszValue_n = wcslen(pszValue);
+	
+	CRM(GetDreamPath(pszDreamPath), "Failed to acquire dream path");
+	pszDreamPath_n = wcslen(pszDreamPath);
+
+	n_pszPath_n = pszDreamPath_n + 1 + pszValue_n + 1;
+	n_pszPath = new wchar_t[n_pszPath_n];
+	memset(n_pszPath, 0, sizeof(wchar_t) * n_pszPath_n);
+
+	// Compose the path
+	// TODO: Maybe do some lower level parsing here since ./ will just get attached
+	err = wcsncat_s(n_pszPath, n_pszPath_n, pszDreamPath, pszDreamPath_n);		
+	err = wcsncat_s(n_pszPath, n_pszPath_n, pszValue, pszValue_n);
+
+Error:
+	return r;
+}
--- DreamOS/Sandbox/Windows/Win64PathManager.cpp
+++ DreamOS/Sandbox/Windows/Win64PathManager.cpp
@@ -1,5 +1,6 @@
 #include "Win64PathManager.h"
 #include <stdlib.h>
+#include <cstring>
 
 Win64PathManager::Win64PathManager() :
 	PathManager()	// Call super
@@ -12,6 +13,9 @@ Win64PathManager::Win64PathManager() :
 
 	memset(m_pszShadersPath, 0, (sizeof(wchar_t) * MAX_PATH));
 	m_pszShadersPath_n = 0;
+
+	// Set up the path values
+	
 }
 
 
@@ -24,13 +28,66 @@ Error:
 	return r;
 }
 
+/*
+RESULT Win64PathManager::RegisterPath(wchar_t *pszName, wchar_t *pszValue) {
+	RESULT r = R_PASS;
+
+	DEBUG_LINEOUT("Found Name:%S Value:%S", pszName, pszValue);
+
+Error:
+	return r;
+}
+*/
+
 // This file runs in current directory
 RESULT Win64PathManager::OpenDreamPathsFile() {
 	RESULT r = R_PASS;
 
 	FILE *pFile = NULL;
+	wchar_t szLine[MAX_PATH];
+
 	errno_t err = fopen_s(&pFile, DREAM_OS_PATHS_FILE, "r");
 	CNM(pFile, "OpenDreamPathsFile failed to open file %S%s", m_pszCurDirectiory, DREAM_OS_PATHS_FILE);
+	rewind(pFile);
+
+	while(fgetws(szLine, MAX_PATH, pFile) != NULL) {
+		wchar_t *pszContext = NULL;
+		wchar_t *pszName = wcstok_s(szLine, _T(" \t\n\v\f\r"), &pszContext);
+		
+		if (pszName == NULL || pszName[0] == '#')
+			continue;
+		
+		wchar_t *pszValue = wcstok_s(NULL, _T(" \t\n\v\f\r"), &pszContext);
+		CRM(RegisterPath(pszName, pszValue), "Failed to register value %S", pszName);
+	}
+		
+Error:
+	if (pFile != NULL) {
+		fclose(pFile);
+		pFile = NULL;
+	}
+	return r;
+}
+
+RESULT Win64PathManager::GetCurrentPath(wchar_t *& pszCurrentPath) {
+	RESULT r = R_PASS;
+
+	pszCurrentPath = NULL;
+
+	CNM(m_pszCurDirectiory, "CurDirectory NULL");
+	pszCurrentPath = m_pszCurDirectiory;
+	
+Error:
+	return r;
+}
+
+RESULT Win64PathManager::GetDreamPath(wchar_t*&pszDreamPath) {
+	RESULT r = R_PASS;
+
+	pszDreamPath = NULL;
+
+	CNM(m_pszDreamRootPath, "DreamRootPath NULL");
+	pszDreamPath = m_pszDreamRootPath;
 
 Error:
 	return r;
