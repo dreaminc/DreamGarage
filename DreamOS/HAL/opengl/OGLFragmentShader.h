d550418baec3d531e517b7fbe16e7d613da87f41
Idan Beck <idanbeck@gmail.com>
2016-02-07 19:05:31 -0800

    OGLFrag/Vert shaders implemented and being used in the implementation

---
+++ DreamOS/HAL/opengl/OGLFragmentShader.h
@@ -0,0 +1,31 @@
+#ifndef OGL_FRAGMENT_SHADER_H_
+#define OGL_FRAGMENT_SHADER_H_
+
+// Dream OS
+// DreamOS/HAL/opengl/OGLFragmentShader.h
+// This is a OGL fragment shader object
+
+#include "OpenGLShader.h"
+
+class OpenGLImp;	// Declare OpenGLImp class
+
+#define FRAGMENT_SHADER_COLOR_INDEX 0
+
+class OGLFragmentShader : public OpenGLShader {
+public:
+	OGLFragmentShader(OpenGLImp *pParentImp);
+
+	// This is handled by the parent class 
+	//~OGLVertexShader(void);
+
+public:
+	const char *GetColorAttributeName() {
+		return "inF_vec3Color";
+	}
+
+	GLuint GetColorIndex() {
+		return FRAGMENT_SHADER_COLOR_INDEX;
+	}
+};
+
+#endif // ! OGL_FRAGMENT_SHADER_H_
---
+++ DreamOS/HAL/opengl/OGLVertexShader.cpp
@@ -0,0 +1,8 @@
+#include "OGLVertexShader.h"
+#include "OpenGLImp.h"
+
+OGLVertexShader::OGLVertexShader(OpenGLImp *pParentImp) :
+	OpenGLShader(pParentImp, GL_VERTEX_SHADER)
+{
+	/* empty stub */
+}
