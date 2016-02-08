885cd93f67b3ea25459bc0efd2a8577e02cab688
Idan Beck <idanbeck@gmail.com>
2016-02-08 15:27:24 -0800

    Subtraction working

--- DreamOS/HAL/opengl/OpenGLImp.cpp
+++ DreamOS/HAL/opengl/OpenGLImp.cpp
@@ -446,7 +446,7 @@ RESULT OpenGLImp::SetData() {
 
 	//(A + B).PrintMatrix();
 	//TranslationMatrix C = (TranslationMatrix&)(A + B);
-	matrix<translate_precision, 4, 4> C = (A + B);
+	matrix<translate_precision, 4, 4> C = (A - B);
 	C.PrintMatrix();
 
 Error:
