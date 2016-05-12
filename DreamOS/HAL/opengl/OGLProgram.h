356a7aadba84a90bdfc4780ffa22597bc40e01ad
Idan Beck <idanbeck@gmail.com>
2016-05-12 16:05:21 -0700

    adding OGLProgram object

---
+++ DreamOS/HAL/opengl/OGLProgram.h
@@ -0,0 +1,19 @@
+#ifndef OGL_PROGRAM_H_
+#define OGL_PROGRAM_H_
+
+#include "RESULT/EHM.h"
+
+// DREAM OS
+// DreamOS/HAL/opengl/OGLProgram.h
+// OpenGL Program class
+
+#include "OpenGLCommon.h"
+
+class OGLProgram {
+public:
+
+private:
+
+};
+
+#endif // ! OGL_PROGRAM_H_
