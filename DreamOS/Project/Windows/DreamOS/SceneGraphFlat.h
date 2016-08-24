dd7fd30cce9c147b5a99f078d20cc439342b4fab
jaisrael <jason.a.israel@gmail.com>
2016-08-24 10:40:32 -0700

    New scene graph or whatever

---
+++ DreamOS/Project/Windows/DreamOS/SceneGraphFlat.h
@@ -0,0 +1,14 @@
+#ifndef SCENE_GRAPH_FLAT_H_
+#define SCENE_GRAPH_FLAT_H_
+
+#include "RESULT/EHM.h"
+#include "Scene/SceneGraph.h"
+
+class SceneGraphFlat : public SceneGraph {
+
+public:
+
+	SceneGraphFlat();
+	~SceneGraphFlat();
+
+};
