777c909ef984d32212b57229bfeecf4f85f65312
Idan Beck <idanbeck@gmail.com>
2016-10-06 19:41:36 -0700

    apparently did not check these files in yet

---
+++ DreamOS/Primitives/volume.cpp
@@ -0,0 +1,232 @@
+#include "volume.h"
+
+volume::volume(double width, double length, double height, bool fTriangleBased) :
+	m_volumeType(RECTANGULAR_CUBOID),
+	m_fTriangleBased(fTriangleBased)
+{
+	RESULT r = R_PASS;
+	CR(Allocate());
+
+	if (width == length &&
+		width == height &&
+		length == height)
+		m_volumeType = CUBE;
+
+	CR(SetVolumeVertices(width, length, height, m_fTriangleBased));
+
+	Validate();
+	return;
+Error:
+	Invalidate();
+	return;
+}
+
+volume::volume(double side, bool fTriangleBased) :
+	m_volumeType(CUBE),
+	m_fTriangleBased(fTriangleBased)
+{
+	RESULT r = R_PASS;
+	CR(Allocate());
+
+	CR(SetVolumeVertices(side, side, side, m_fTriangleBased));
+
+	Validate();
+	return;
+Error:
+	Invalidate();
+	return;
+}
+
+RESULT volume::Allocate() {
+	RESULT r = R_PASS;
+
+	CR(AllocateVertices(NUM_VOLUME_POINTS));
+
+	if (m_fTriangleBased) {
+		CR(AllocateTriangleIndexGroups(NUM_VOLUME_TRIS));
+	}
+	else {
+		CR(AllocateQuadIndexGroups(NUM_VOLUME_QUADS));
+	}
+
+Error:
+	return R_PASS;
+}
+
+unsigned int volume::NumberVertices() {
+	return NUM_VOLUME_POINTS;
+}
+
+unsigned int volume::NumberIndices() {
+	//return NUM_VOLUME_INDICES;
+	if (m_fTriangleBased) {
+		return NUM_VOLUME_TRI_INDICES;
+	}
+	else {
+		return NUM_VOLUME_QUAD_INDICES;
+	}
+}
+
+RESULT volume::SetVolumeVertices(double width, double length, double height, bool fTriangleBased) {
+	RESULT r = R_PASS;
+
+	point_precision halfWidth = static_cast<point_precision>(length / 2.0f);
+	point_precision halfLength = static_cast<point_precision>(width / 2.0f);
+	point_precision halfHeight = static_cast<point_precision>(height / 2.0f);
+	int vertCount = 0;
+	int triCount = 0;
+	int quadCount = 0;
+	int TL, TR, BL, BR;
+
+	// Set up indices 
+	TriangleIndexGroup *pTriIndices = reinterpret_cast<TriangleIndexGroup*>(m_pIndices);
+	QuadIndexGroup *pQuadIndices = reinterpret_cast<QuadIndexGroup*>(m_pIndices);
+
+	// OpenGL is default counter clockwise
+
+	///*
+	// Front face
+	m_pVertices[TR = vertCount++] = vertex(point(halfWidth, halfHeight, halfLength), vector(0.0f, 0.0f, 1.0f), uvcoord(1.0f, 1.0f));		// A top left
+	m_pVertices[TL = vertCount++] = vertex(point(-halfWidth, halfHeight, halfLength), vector(0.0f, 0.0f, 1.0f), uvcoord(0.0f, 1.0f));		// B top right
+	m_pVertices[BR = vertCount++] = vertex(point(halfWidth, -halfHeight, halfLength), vector(0.0f, 0.0f, 1.0f), uvcoord(1.0f, 0.0f));		// C bottom left
+	m_pVertices[BL = vertCount++] = vertex(point(-halfWidth, -halfHeight, halfLength), vector(0.0f, 0.0f, 1.0f), uvcoord(0.0f, 0.0f));		// D bottom right
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetTriangleTangentBitangent(BL, BR, TR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR, vector::jVector(1.0f), vector::iVector(1.0f));
+
+	//*/
+
+	///*
+	// Back face
+	m_pVertices[TL = vertCount++] = vertex(point(halfWidth, halfHeight, -halfLength), vector(0.0f, 0.0f, -1.0f), uvcoord(0.0f, 1.0f));		// A
+	m_pVertices[TR = vertCount++] = vertex(point(-halfWidth, halfHeight, -halfLength), vector(0.0f, 0.0f, -1.0f), uvcoord(1.0f, 1.0f));		// B
+	m_pVertices[BL = vertCount++] = vertex(point(halfWidth, -halfHeight, -halfLength), vector(0.0f, 0.0f, -1.0f), uvcoord(0.0f, 0.0f));		// C
+	m_pVertices[BR = vertCount++] = vertex(point(-halfWidth, -halfHeight, -halfLength), vector(0.0f, 0.0f, -1.0f), uvcoord(1.0f, 0.0f));		// D
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR, vector::jVector(1.0f), vector::iVector(-1.0f));
+	//*/
+
+	///*
+	// Left face
+	m_pVertices[TL = vertCount++] = vertex(point(halfWidth, -halfHeight, halfLength), vector(1.0f, 0.0f, 0.0f), uvcoord(0.0f, 1.0f));		// A
+	m_pVertices[TR = vertCount++] = vertex(point(halfWidth, halfHeight, halfLength), vector(1.0f, 0.0f, 0.0f), uvcoord(1.0f, 1.0f));		// B
+	m_pVertices[BL = vertCount++] = vertex(point(halfWidth, -halfHeight, -halfLength), vector(1.0f, 0.0f, 0.0f), uvcoord(0.0f, 0.0f));		// C
+	m_pVertices[BR = vertCount++] = vertex(point(halfWidth, halfHeight, -halfLength), vector(1.0f, 0.0f, 0.0f), uvcoord(1.0f, 0.0f));		// D
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR, vector::jVector(1.0f), vector::kVector(1.0f));
+	//*/
+
+	///*
+	// Right face
+	m_pVertices[TR = vertCount++] = vertex(point(-halfWidth, -halfHeight, halfLength), vector(-1.0f, 0.0f, 0.0f), uvcoord(1.0f, 1.0f));		// A
+	m_pVertices[TL = vertCount++] = vertex(point(-halfWidth, halfHeight, halfLength), vector(-1.0f, 0.0f, 0.0f), uvcoord(0.0f, 1.0f));		// B
+	m_pVertices[BR = vertCount++] = vertex(point(-halfWidth, -halfHeight, -halfLength), vector(-1.0f, 0.0f, 0.0f), uvcoord(1.0f, 0.0f));		// C
+	m_pVertices[BL = vertCount++] = vertex(point(-halfWidth, halfHeight, -halfLength), vector(-1.0f, 0.0f, 0.0f), uvcoord(0.0f, 0.0f));		// D
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR, vector::jVector(1.0f), vector::kVector(-1.0f));
+	//*/
+
+	///*
+	// Top face
+	m_pVertices[TR = vertCount++] = vertex(point(-halfWidth, halfHeight, halfLength), vector(0.0f, 1.0f, 0.0f), uvcoord(1.0f, 1.0f));		// A
+	m_pVertices[TL = vertCount++] = vertex(point(halfWidth, halfHeight, halfLength), vector(0.0f, 1.0f, 0.0f), uvcoord(0.0f, 1.0f));			// B
+	m_pVertices[BR = vertCount++] = vertex(point(-halfWidth, halfHeight, -halfLength), vector(0.0f, 1.0f, 0.0f), uvcoord(1.0f, 0.0f));		// C
+	m_pVertices[BL = vertCount++] = vertex(point(halfWidth, halfHeight, -halfLength), vector(0.0f, 1.0f, 0.0f), uvcoord(0.0f, 0.0f));		// D
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//SetQuadTangentBitangent(TL, TR, BL, BR, vector::jVector(1.0f), vector::iVector(-1.0f));
+	//*/
+
+	///*
+	// Bottom face
+	m_pVertices[TL = vertCount++] = vertex(point(-halfWidth, -halfHeight, halfLength), vector(0.0f, -1.0f, 0.0f), uvcoord(0.0f, 1.0f));		// A
+	m_pVertices[TR = vertCount++] = vertex(point(halfWidth, -halfHeight, halfLength), vector(0.0f, -1.0f, 0.0f), uvcoord(1.0f, 1.0f));		// B
+	m_pVertices[BL = vertCount++] = vertex(point(-halfWidth, -halfHeight, -halfLength), vector(0.0f, -1.0f, 0.0f), uvcoord(0.0f, 0.0f));		// C
+	m_pVertices[BR = vertCount++] = vertex(point(halfWidth, -halfHeight, -halfLength), vector(0.0f, -1.0f, 0.0f), uvcoord(1.0f, 0.0f));		// D
+
+	if (fTriangleBased) {
+		pTriIndices[triCount++] = TriangleIndexGroup(BL, BR, TR);
+		SetTriangleTangentBitangent(BL, BR, TR);
+
+		pTriIndices[triCount++] = TriangleIndexGroup(TR, TL, BL);
+		SetTriangleTangentBitangent(TR, TL, BL);
+	}
+	else {
+		pQuadIndices[quadCount++] = QuadIndexGroup(TR, TL, BL, BR);
+		SetQuadTangentBitangent(TL, TR, BL, BR);
+	}
+
+	//SetQuadTangentBitangent(TL, TR, BL, BR);
+	//*/
+
+	//	Error:
+	return r;
+}
+
+bool volume::IsTriangleBased() {
+	return m_fTriangleBased;
+}
---
+++ DreamTestApp.cpp
@@ -0,0 +1,67 @@
+#include "Logger/Logger.h"
+#include "DreamTestApp.h"
+#include <string>
+
+// TODO make it possible to have different Dream Applications, then split the TESTING code into a new app
+//#define TESTING
+
+RESULT DreamTestApp::LoadScene() {
+	RESULT r = R_PASS;
+
+	// IO
+	//RegisterSubscriber((SK_SCAN_CODE)('C'), this);
+
+	CmdPrompt::GetCmdPrompt()->RegisterMethod(CmdPrompt::method::DreamApp, this);
+
+	AddSkybox();
+
+	light *pLight = AddLight(LIGHT_DIRECITONAL, 1.0f, point(0.0f, 10.0f, 0.0f), color(COLOR_WHITE), color(COLOR_WHITE), vector(-0.2f, -1.0f, -0.5f));
+	//pLight->EnableShadows();
+
+
+	quad *pBQuad = AddQuad(10.0f, 20.0f, 200, 200);// , pHeightTextureCobble);
+	pBQuad->MoveTo(point(0.0f, -1.0f, 0.0f));
+
+	sphere *pSphere = AddSphere(0.5f, 10, 10, color(COLOR_RED));
+	pSphere->MoveTo(1.5f, 0.5f, 0.0f);
+	
+	volume *pVolume = AddVolume(0.5f, false);
+	pVolume->MoveTo(-1.5f, 0.5f, 0.0f);
+	pVolume->SetWireframe(true);
+
+	//Error:
+	return r;
+}
+
+RESULT DreamTestApp::Update(void) {
+	RESULT r = R_PASS;
+
+
+	//Error:
+	return r;
+}
+
+RESULT DreamTestApp::Notify(SenseKeyboardEvent *kbEvent) {
+	RESULT r = R_PASS;
+	/*
+	switch (kbEvent->KeyCode) {
+	case (SK_SCAN_CODE)('C') : {
+	if (kbEvent->KeyState != 0) {
+	HUD_OUT("Key 'C' is pressed - switch model");
+	SendSwitchHeadMessage();
+	}
+	}
+	}
+	*/
+	//Error:
+	return r;
+}
+
+RESULT DreamTestApp::Notify(CmdPromptEvent *event) {
+	RESULT r = R_PASS;
+
+	HUD_OUT("DreamAPP command");
+
+//Error:
+	return r;
+}
