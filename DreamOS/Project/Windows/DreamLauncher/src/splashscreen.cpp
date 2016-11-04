b75418b4d9b832abec71ea091c4e1ed2c7f61140
Nirosta <nirosta@dreamos.com>
2016-11-03 19:12:45 -0700

    Moving to embedded resource for splash gif

--- DreamOS/Project/Windows/DreamLauncher/src/splashscreen.cpp
+++ DreamOS/Project/Windows/DreamLauncher/src/splashscreen.cpp
@@ -3,7 +3,7 @@
 
 #include <wx/wxprec.h>
 #include <wx/animate.h>
-#include <wx/wfstream.h>
+#include <wx/mstream.h>
 
 class SplashApp : public wxApp, public WindowController
 {
@@ -33,15 +33,47 @@ bool SplashApp::Initialize(int& argc, wxChar **argv)
 	return wxApp::Initialize(argc, argv);
 }
 
+bool LoadDataFromResource(char*& t_data, DWORD& t_dataSize, const wxString& t_name)
+{
+	bool     result = false;
+	HGLOBAL  resHandle = 0;
+	HRSRC    resource;
+
+	resource = FindResource(0, t_name.wchar_str(), RT_RCDATA);
+
+	if (resource != 0)
+	{
+		resHandle = LoadResource(NULL, resource);
+
+		if (resHandle != 0)
+		{
+			t_data = (char*)LockResource(resHandle);
+			t_dataSize = SizeofResource(NULL, resource);
+			result = true;
+		}
+	}
+
+	return result;
+}
+
 bool SplashApp::OnInit()
 {
 	m_frame = new wxFrame(NULL, wxID_ANY, "DreamSplash", wxDefaultPosition, wxSize(350, 350), wxSYSTEM_MENU);
 
 	wxAnimationCtrl* animation = new wxAnimationCtrl(m_frame, wxID_ANY, wxNullAnimation, wxPoint(-225, -125));
 
-	wxFileInputStream splashFile("splash.gif");
+	char* data = nullptr;
+	DWORD	size = 0;
 
-	animation->Load(splashFile);
+	if (!LoadDataFromResource(data, size, "SPLASH_LOADING_GIF"))
+	{
+		LOG(ERROR) << "resource not found";
+		return false;
+	}
+
+	wxMemoryInputStream splash(data, size);
+
+	animation->Load(splash);
 	animation->Play();
 	m_frame->Center();
 	m_frame->Show(true);
