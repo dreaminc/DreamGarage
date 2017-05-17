27b85604559db4f7068124bf273ac431a66c2d43
jaisrael <jason.a.israel@gmail.com>
2017-05-17 13:42:41 -0700

    OGLText source file

---
+++ DreamOS/HAL/opengl/OGLText.cpp
@@ -0,0 +1,45 @@
+#include "OGLText.h"
+
+
+OGLText::OGLText(OpenGLImp *pParentImp, std::shared_ptr<Font> pFont, const std::string& text, double size, bool isBillboard) :
+	text(pFont, text, size, isBillboard),
+	OGLObj(pParentImp)
+{
+	// TODO: Implement valid and CV EHM
+	RESULT r = OGLInitialize();
+
+	std::wstring font(L"Fonts/" + pFont->GetGlyphImageFile());
+	// Load appropriate glyph texture
+
+	texture *pColorTexture = new OGLTexture(pParentImp, (wchar_t*)font.c_str(), texture::TEXTURE_TYPE::TEXTURE_COLOR);
+	SetColorTexture(pColorTexture);
+}
+
+OGLText::OGLText(OpenGLImp *pParentImp, std::shared_ptr<Font> pFont, texture *pFontTexture, const std::string& text, double size, bool isBillboard) :
+	text(pFont, text, size, isBillboard),
+	OGLObj(pParentImp)
+{
+	// TODO: Implement valid and CV EHM
+	RESULT r = OGLInitialize();
+
+	SetColorTexture(pFontTexture);
+}
+
+OGLText* OGLText::SetText(const std::string& text, double size)
+{
+	bool hasChanged = false;
+
+	text::SetText(text, size, &hasChanged);
+
+	// TODO: need to be able to deal with changing vertex amounts automatically
+	if (CheckAndCleanDirty())
+	{
+		ReleaseOGLBuffers();
+		OGLInitialize();
+	}
+	
+	if (hasChanged)
+		SetDirty();
+
+	return this;
+}
