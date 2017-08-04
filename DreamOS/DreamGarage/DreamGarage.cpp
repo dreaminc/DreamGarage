8998a7360fca6f31cc9c5177c4e545ac411efb97
idanbeck <idanbeck@gmail.com>
2017-08-03 19:47:25 -0700

    dream browser quick fix

--- DreamOS/DreamGarage/DreamGarage.cpp
+++ DreamOS/DreamGarage/DreamGarage.cpp
@@ -807,7 +807,7 @@ RESULT DreamGarage::OnEnvironmentAsset(std::shared_ptr<EnvironmentAsset> pEnviro
 	//*/
 	if (m_pDreamBrowser != nullptr) {
 		m_pDreamBrowser->SetVisible(true);
-		m_pDreamBrowser->FadeQuadToBlack();
+		//m_pDreamBrowser->FadeQuadToBlack();
 		m_pDreamBrowser->SetEnvironmentAsset(pEnvironmentAsset);
 	}
 	return r;
