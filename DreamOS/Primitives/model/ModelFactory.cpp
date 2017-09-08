02c12630658b6a84d1dc8f37f4d151a635b8f26d
idanbeck <idanbeck@gmail.com>
2017-09-08 14:04:35 -0700

    asset importer now loading in files from path - working, now to push into the proper model arch and design

--- DreamOS/Primitives/model/ModelFactory.cpp
+++ DreamOS/Primitives/model/ModelFactory.cpp
@@ -11,6 +11,8 @@
 #include "model.h"
 #include "HAL/HALImp.h"
 
+#include "Core/Utilities.h"
+
 model* ModelFactory::MakeModel(HALImp *pParentImp, std::wstring wstrModelFilename) {
 	RESULT r = R_PASS;
 
@@ -25,8 +27,11 @@ model* ModelFactory::MakeModel(HALImp *pParentImp, std::wstring wstrModelFilenam
 	CRM(pPathManager->DoesPathExist(wstrModelFilePath, true), "Model file path not found");
 
 	// Load model from disk
+	const aiScene *pScene = assetImporter.ReadFile(util::WideStringToString(wstrModelFilePath), aiProcess_Triangulate | aiProcess_GenNormals | aiProcess_FlipUVs);
 
-	
+	CNM(pScene, "Asset Importer failed to allocate scene: %s", assetImporter.GetErrorString());
+	CBM(((pScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE) == 0), "Asset Importer Scene Incomplete: %s", assetImporter.GetErrorString());
+	CNM(pScene->mRootNode, "Asset Importer scene root is null: %s", assetImporter.GetErrorString());
 
 
 // Success:
