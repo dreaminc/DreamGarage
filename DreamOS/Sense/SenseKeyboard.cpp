137da28a666f055eba01c49f9fd10d4e7a371226
Idan Beck <idanbeck@incidenttech.com>
2017-10-09 17:33:06 -0700

    Feature/vs2017 (#289)
    * moving to vs2017 - not clear what else is broken, need to test a full build
    
    * release version - tested a development push
    
    * had to update wxwidgets to the new VS2017 toolset - this is going to go soon however

--- DreamOS/Sense/SenseKeyboard.cpp
+++ DreamOS/Sense/SenseKeyboard.cpp
@@ -15,19 +15,17 @@
 #include <locale>
 #include <codecvt>
 
-#if _MSC_VER == 1900
-std::string utf16_to_utf8(std::u16string utf16_string)
-{
-	std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t> convert;
-	auto p = reinterpret_cast<const int16_t *>(utf16_string.data());
-	return convert.to_bytes(p, p + utf16_string.size());
-}
+#if _MSC_VER >= 1900
+	std::string utf16_to_utf8(std::u16string utf16_string) {
+		std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t> convert;
+		auto p = reinterpret_cast<const int16_t *>(utf16_string.data());
+		return convert.to_bytes(p, p + utf16_string.size());
+	}
 #else
-std::string utf16_to_utf8(std::u16string utf16_string)
-{
-	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
-	return convert.to_bytes(utf16_string);
-}
+	std::string utf16_to_utf8(std::u16string utf16_string) {
+		std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+		return convert.to_bytes(utf16_string);
+	}
 #endif
 
 
