7bb87bbe0188be99402a5e200da4d9ea62462df4
Idan Beck <idan@dreamos.com>
2017-12-18 19:15:02 -0800

    downloading now works, now need to ensure launch and test out end to end deploy

--- DreamOS/Project/Windows/DreamLaunch/App.xaml.cs
+++ DreamOS/Project/Windows/DreamLaunch/App.xaml.cs
@@ -16,12 +16,13 @@ namespace DreamLaunch {
     
     public partial class App : Application
     {
-
         // Update Manager Handlers 
         private void HandleOnFirstRun()
         {
             // do nothing
             //m_fShowTheWelcomeWizard = true;
+            if (m_mainWindow != null)
+                m_mainWindow.SetStatusText("First run");
         }
 
         private void HandleOnInitialInstall(Version ver)
@@ -35,11 +36,17 @@ namespace DreamLaunch {
             // Do nothing for now
             //if (m_squirrelUpdateManager != null)
             //    m_squirrelUpdateManager.CreateShortcutForThisExe()
+
+            if (m_mainWindow != null)
+                m_mainWindow.SetStatusText("Updating");
+
         }
 
         private void HandleOnAppObsoleted(Version ver)
         {
             // Do nothing for now
+            if (m_mainWindow != null)
+                m_mainWindow.SetStatusText("OnObsolete");
         }
 
         private void HandleOnAppUninstall(Version ver)
@@ -47,35 +54,86 @@ namespace DreamLaunch {
             // Nothing for now
             //if (m_squirrelUpdateManager != null)
             //  m_squirrelUpdateManager.RemoveShortcutForThisExe()
+
+            if (m_mainWindow != null)
+                m_mainWindow.SetStatusText("App Uninstall");
+        }
+
+        int s_downloadCounter = 0;
+        private void HandleOnUpdateAppProgress(int value)
+        {
+            if (value == 100)
+            {
+                if (m_mainWindow != null)
+                    m_mainWindow.SetStatusText("Finished Downloading");
+            }
+            else
+            {
+
+                // Download progress
+                s_downloadCounter++;
+                if (s_downloadCounter >= 3)
+                    s_downloadCounter = 0;
+
+                if (m_mainWindow != null)
+                {
+                    // Getting fancy
+                    string strDownload = string.Format("Downloading {0}% ", value);
+                    for (int i = 0; i < s_downloadCounter; i++)
+                        strDownload += ".";
+
+                    m_mainWindow.SetStatusText(strDownload);
+                    m_mainWindow.SetDownloadProgressBarPercentage(value);
+                }
+            }
         }
 
         async private void CheckForSquirrelUpdate()
         {
             try
             {
-                await m_squirrelUpdateManager.UpdateApp();
+                if (m_mainWindow != null)
+                    m_mainWindow.SetStatusText("Checking for Update");
+
+                await m_squirrelUpdateManager.UpdateApp(progress: HandleOnUpdateAppProgress);
             }
             catch(Exception ex)
             {
+                if (m_mainWindow != null)
+                    m_mainWindow.SetStatusText("Error");
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 throw new ApplicationException(string.Format("Failed to call UpdatEManager::UpdateApp"), ex);
             }
         }
 
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private void HandleMainWindowContentRendered(object sender, System.EventArgs eventArgs)
         {
             // Figure out where to look for RELEASES
 
 #if (_TEST_LOCAL_LAUNCHER)
-            m_strReleasesURI = "C:\\dev\\DreamGarage\\DreamOS\\Project\\Windows\\DreamOS\\DevReleases";
+            //m_strReleasesURI = "C:\\dev\\DreamGarage\\DreamOS\\Project\\Windows\\DreamOS\\DevReleases";
+            m_strReleasesURI = "https://github.com/dreaminc/Dream/releases/download/DevReleases/";
+#elif (_DEV_RELEASE)
+            m_strReleasesURI = "https://github.com/dreaminc/Dream/releases/download/DevReleases/";
+#elif (_PROD_RELEASE)
+            m_strReleasesURI = "https://github.com/dreaminc/Dream/releases/download/Releases/";
 #endif
 
-            if (m_squirrelUpdateManager == null) {
+            if (m_squirrelUpdateManager == null)
+            {
                 try
                 {
+                    if (m_mainWindow != null)
+                        m_mainWindow.SetStatusText("Initializing");
                     m_squirrelUpdateManager = new UpdateManager(m_strReleasesURI);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
+                    if (m_mainWindow != null)
+                        m_mainWindow.SetStatusText("Checking for Update");
+
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     throw new ApplicationException(string.Format("Failed to create Squirrel UpdateManager"), ex);
                 }
             }
@@ -89,10 +147,19 @@ namespace DreamLaunch {
               onAppObsoleted: HandleOnAppObsoleted);
 
             CheckForSquirrelUpdate();
+        }
 
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // First launch the main window
+            m_mainWindow = new MainWindow();
+            m_mainWindow.Show();
+
+            m_mainWindow.ContentRendered += HandleMainWindowContentRendered;
         }
 
         //private bool m_fShowTheWelcomeWizard = true;
+        private MainWindow m_mainWindow = null;
         private UpdateManager m_squirrelUpdateManager = null;
         private string m_strReleasesURI = null;
     }
--- DreamOS/Project/Windows/DreamLaunch/MainWindow.xaml.cs
+++ DreamOS/Project/Windows/DreamLaunch/MainWindow.xaml.cs
@@ -20,12 +20,44 @@ namespace DreamLaunch {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
+    
     public partial class MainWindow : Window {
 
         public MainWindow() {
             InitializeComponent();
+
+            SetStatusText("Loading");
         }
 
+        public void SetStatusText(string strStatus)
+        {
+            m_textBlockStatus.Dispatcher.BeginInvoke(
+                (Action)(() => { m_textBlockStatus.Text = strStatus; })
+            );
+        }
+
+        public void SetDownloadProgressBarValue(int value)
+        {
+            m_progressBarDownload.Dispatcher.BeginInvoke(
+                (Action)(() => { m_progressBarDownload.Value = value; })
+            );
+        }
+
+        public void SetDownloadProgressBarPercentage(int percentage)
+        {
+            if (percentage > 100)
+                percentage = 100;
+            else if (percentage < 0)
+                percentage = 0;
+
+            m_progressBarDownload.Dispatcher.BeginInvoke(
+                (Action)(() => {
+                    int val = (int)((float)(m_progressBarDownload.Maximum) * ((float)(percentage) / 100.0f));
+                    m_progressBarDownload.Value = val;
+                }) 
+            );
+        }
+
+        //TextBlock m_textBlockStatus = null;
     }
 }
