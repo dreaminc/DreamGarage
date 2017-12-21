fbe68fef294b7e06d45581a685dbaf7fe6c2d1c6
Idan Beck <idanbeck@incidenttech.com>
2017-12-21 12:48:27 -0800

    Feature/fixupdater (#362)
    * Adding new DreamLaunch WPF app that will replace DreamLauncher (native)
    
    * Working, DreamLaunch.exe is copied to a replica named DreamLauncher.exe which will overwrite the old launcher in cases where we need to propagate / someone hasn't launched or reinstalled in a bit
    
    * Removed DreamSetup project and added a .gitignore

---
+++ DreamOS/Project/Windows/DreamLaunch/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+namespace DreamLaunch {
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    
+    public partial class App : Application {
+        private async void HandleMainWindowContentRenderedAsync(object sender, System.EventArgs eventArgs) {
+
+            // Set up Squirrel Controller
+            m_squirrelUpdateController.SetMainWindowController(m_mainWindow);
+            m_squirrelUpdateController.InitializeSquirrel();
+
+            // Set up Registry
+            m_dreamRegistryController.SetMainWindowController(m_mainWindow);
+            m_dreamRegistryController.SetDreamRootAppDirectory(m_squirrelUpdateController.GetSquirrelRootAppDirectory());
+
+            if (m_dreamRegistryController.Initialize() == -1) {
+                Environment.Exit(0);
+            }
+
+            // Kick off an Update
+            int result = await m_squirrelUpdateController.CheckForSquirrelUpdate();
+
+            if(result == -1) {
+                //MessageBox.Show("Squirrel Update Failed", "DreamLaunch", MessageBoxButton.OK, MessageBoxImage.Information);
+                Shutdown();
+                return;
+            }
+
+            // Don't launch on first install or if registry updated
+            // Not sure if this is the best way (if registry is updated, app will not launch post initial install)
+            //if (m_squirrelUpdateController.IsInitialInstall() || m_dreamRegistryController.DidRegistryUpdate()) {
+
+            // This is a bit of a hack but should work
+            string strCommandLineArgs = "";
+            string[] commandLineArguments = Environment.GetCommandLineArgs();
+            commandLineArguments = commandLineArguments.Skip(1).ToArray();
+            foreach (string arg in commandLineArguments) {
+                strCommandLineArgs += String.Format("\"{0}\" ", arg);
+            }
+
+            strCommandLineArgs.Trim();
+            strCommandLineArgs.ToLower();
+
+            string strSquirrelFirstRun = "--squirrel-firstrun";
+            strSquirrelFirstRun.Trim();
+            strSquirrelFirstRun.ToLower();
+
+            //MessageBox.Show(string.Format("{0} is {1} to {2}", strCommandLineArgs, strCommandLineArgs.IndexOf(strSquirrelFirstRun), strSquirrelFirstRun), 
+            //    "DreamLaunch", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            if (m_squirrelUpdateController.IsInitialInstall() || strCommandLineArgs.IndexOf(strSquirrelFirstRun) != -1) {
+
+                m_mainWindow.SetStatusText("Dream Installed!");
+
+                // TEST
+                //MessageBox.Show("Initial Install Complete", "DreamLaunch", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Persist install message
+                Thread.Sleep(1250);
+            }
+            else {
+                // Launch DreamOS Process
+                m_dreamOSLauncher.SetMainWindowController(m_mainWindow);
+                m_dreamOSLauncher.Initialize();
+
+                // Launch Dream OS (not on first install)
+                m_dreamOSLauncher.LaunchDreamOS();
+            }
+
+            // Exit
+            //Environment.Exit(0);
+            Shutdown();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e) {
+
+            m_squirrelUpdateController = new SquirrelUpdateController();
+            m_dreamRegistryController = new DreamRegistryController();
+            m_dreamOSLauncher = new DreamOSLauncher();
+
+            // First launch the main window
+            m_mainWindow = new MainWindow();
+
+            m_mainWindow.ContentRendered += HandleMainWindowContentRenderedAsync;
+
+            m_mainWindow.Show();
+
+            
+        }
+
+        //private bool m_fShowTheWelcomeWizard = true;
+        private MainWindow m_mainWindow = null;
+
+        private SquirrelUpdateController m_squirrelUpdateController = null;
+        private DreamRegistryController m_dreamRegistryController = null;
+        private DreamOSLauncher m_dreamOSLauncher = null;
+    }
+}
---
+++ DreamOS/Project/Windows/DreamLaunch/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Squirrel;
+
+namespace DreamLaunch {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            InitializeComponent();
+
+            SetStatusText("Loading");
+        }
+
+        public void SetStatusText(string strStatus) {
+            m_textBlockStatus.Dispatcher.BeginInvoke(
+                (Action)(() => { m_textBlockStatus.Text = strStatus; })
+            );
+        }
+
+        public void SetDownloadProgressBarValue(int value) {
+            m_progressBarDownload.Dispatcher.BeginInvoke(
+                (Action)(() => { m_progressBarDownload.Value = value; })
+            );
+        }
+
+        public void SetDownloadProgressBarPercentage(int percentage) {
+            if (percentage > 100)
+                percentage = 100;
+            else if (percentage < 0)
+                percentage = 0;
+
+            m_progressBarDownload.Dispatcher.BeginInvoke(
+                (Action)(() => {
+                    int val = (int)((float)(m_progressBarDownload.Maximum) * ((float)(percentage) / 100.0f));
+                    m_progressBarDownload.Value = val;
+                }) 
+            );
+        }
+
+        //TextBlock m_textBlockStatus = null;
+    }
+}
