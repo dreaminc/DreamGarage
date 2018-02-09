2a5382e80d829e6a4ed4b7fc71c92f45e291814a
johnl33 <31673314+JohnL33@users.noreply.github.com>
2018-02-09 13:37:07 -0800

    clean up files

---
+++ DreamOS/Project/Windows/DreamDesktopDuplication/include/DDCIPCMessage.h
@@ -0,0 +1,42 @@
+#ifndef _DDCIPCMSG_H_
+#define _DDCIPCMSG_H_
+
+class DDCIPCMessage {
+
+public:
+	enum class type : unsigned long {
+		PING,
+		ACK,
+		START,
+		STOP,
+		FRAME,
+		INVALID
+	};
+
+	enum class PIXEL_FORMAT {
+		Unspecified, // this will generate an RGB/RGBA based on the number of channels - 1 to 1 with color.h
+		RGB,
+		RGBA,
+		BGR,
+		BGRA,
+		INVALID
+	};
+	
+	DDCIPCMessage::type GetMessage() {
+		return m_body.m_msgType;
+	}
+
+	HRESULT SetType(DDCIPCMessage::type msgType) {
+		m_body.m_msgType = msgType;
+		return S_OK;
+	}
+
+private:
+	__declspec(align(8)) struct MessageBody {
+		DDCIPCMessage::type m_msgType;
+		unsigned char* pDataBuffer;
+		PIXEL_FORMAT pixelFormat;
+	} m_body;
+};
+
+#endif
