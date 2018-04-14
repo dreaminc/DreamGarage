b5d950e9b4bbde6d9f9909a73c073aa929b69b58
jaisrael <jason.a.israel@gmail.com>
2018-04-13 17:01:14 -0700

    polish

--- DreamOS/DreamShareView/MultiContentTestSuite.cpp
+++ DreamOS/DreamShareView/MultiContentTestSuite.cpp
@@ -338,6 +338,9 @@ RESULT MultiContentTestSuite::AddTestActiveSource() {
 			"www.nyt.com",
 			"www.dreamos.com",
 			"en.wikipedia.org/wiki/Tropical_house",
+			"www.nyt.com",
+			"www.dreamos.com",
+			"en.wikipedia.org/wiki/Tropical_house",
 			"www.livelovely.com",
 			"www.twitch.tv"
 		} ;
@@ -393,7 +396,7 @@ RESULT MultiContentTestSuite::AddTestActiveSource() {
 					}
 
 				}
-				//*
+				/*
 				else if (randAction == 9) {
 					DEBUG_LINEOUT("OPEN");
 					int randIndex = std::rand() % pTestContext->strURIs.size();
@@ -408,7 +411,7 @@ RESULT MultiContentTestSuite::AddTestActiveSource() {
 				}
 				//*/
 
-				//*
+				/*
 				else if (randAction == 8) {
 					DEBUG_LINEOUT("CLOSE");
 					auto tabButtons = pTestContext->pUserControlArea->m_pDreamTabView->m_tabButtons;
