9075d1ad36308e8330174ebdc816cf2fb7701001
Idan Beck <idan@dreamos.com>
2018-06-05 17:05:55 -0700

    Logging now workking and configured for all builds

--- DreamOS/RESULT/EHM.h
+++ DreamOS/RESULT/EHM.h
@@ -33,7 +33,9 @@ template <typename T, size_t N> char(&ArraySizeHelper(T(&array)[N]))[N];
 #include "DreamLogger/DreamLogger.h"
 
 // Logging (needs DreamLogger included)
-#define _ENABLE_LOGGING
+// This has been moved to project config, otherwise it breaks across multiple projects
+// This requirement will be fixed when we move away from the monolithic build
+//#define _ENABLE_LOGGING
 #ifdef _ENABLE_LOGGING
 #define DOSLOG(level, strMsg, ...) do { \
 		DreamLogger::instance()->Log(DreamLogger::Level::level, strMsg, ##__VA_ARGS__); \
@@ -104,64 +106,66 @@ template <typename T, size_t N> char(&ArraySizeHelper(T(&array)[N]))[N];
 
 #define LOG_DOS_MSGS
 #ifdef LOG_DOS_MSGS
-	#define DOSLogErrorMessage(level, t, r) DOSLOG(level, "%s:%s(%d):Error: 0x%x\n", t, __FILE__, __LINE__, r)
+	#define DOSLogError(level, crt, r) DOSLOG(level, "%s:%s:%s(%d):Error: 0x%x\n", crt, __FILE__, __FUNCTION__, __LINE__, r)
+	#define DOSLogErrorMessage(level, crt, msg, r) DOSLOG(level, "%s:%s:%s(%d):%s:Error: 0x%x\n", crt, __FILE__, __FUNCTION__, __LINE__, msg, r)
 #else
-	#define DOSLogErrorMessage(level, t, r)  
+	#define DOSLogError(level, crt, r)
+	#define DOSLogErrorMessage(level, crt, msg, r)
 #endif
 
 // Check RESULT value
 // Ensures that RESULT is successful
-#define CR(res) do{r=(res);if(r&0x80000000){DOSLogErrorMessage(ERR, "CR", r);goto Error;}}while(0);
-#define CRM(res, msg, ...) do{r= (res);if(r&0x80000000){DOSLogErrorMessage(ERR, "CRM", r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Error: 0x%x\n",r);goto Error;}}while(0);
+#define CR(res) do{r=(res);if(r&0x80000000){DOSLogError(ERR, "CR", r);goto Error;}}while(0);
+#define CRM(res, msg, ...) do{r= (res);if(r&0x80000000){DOSLogErrorMessage(ERR, "CRM", msg, r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Error: 0x%x\n",r);goto Error;}}while(0);
 
 // Check result no assign (this allows for mapping with non RESULT error types without having to cast
-#define CRNA(res) do{if(res&0x80000000){DOSLogErrorMessage(ERR, "CRNA", r);;r=R_FAIL;goto Error;}}while(0);
-#define CRNAM(res, msg, ...) do{if(res&0x80000000){DOSLogErrorMessage(ERR, "CRNAM", r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Error: 0x%x\n", res);r=R_FAIL;goto Error;}}while(0);
+#define CRNA(res) do{if(res&0x80000000){DOSLogError(ERR, "CRNA", r);;r=R_FAIL;goto Error;}}while(0);
+#define CRNAM(res, msg, ...) do{if(res&0x80000000){DOSLogErrorMessage(ERR, "CRNAM", msg, r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Error: 0x%x\n", res);r=R_FAIL;goto Error;}}while(0);
 
-#define ACRM(res, msg, ...) do{if((res)&0x80000000){DOSLogErrorMessage(ERR, "ACRM", res);;DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0); }}while(0);
-#define ACR(res) do{if((res)&0x80000000){DOSLogErrorMessage(ERR, "ACR", res);;assert(0); }}while(0);
+#define ACRM(res, msg, ...) do{if((res)&0x80000000){DOSLogErrorMessage(ERR, "ACRM", msg, res);;DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0); }}while(0);
+#define ACR(res) do{if((res)&0x80000000){DOSLogError(ERR, "ACR", res);;assert(0); }}while(0);
 
 // Warning
-#define WCR(res) do{r=(res);if(r&0x80000000){DOSLogErrorMessage(WARN, "WCR", r);/*goto Error;*/}}while(0);
-#define WCRM(res, msg, ...) do{r = (res);if(r&0x80000000){DOSLogErrorMessage(WARN, "WCRM", r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Warning: 0x%x\n",r);/*goto Error;*/}}while(0)
+#define WCR(res) do{r=(res);if(r&0x80000000){DOSLogError(WARN, "WCR", r);/*goto Error;*/}}while(0);
+#define WCRM(res, msg, ...) do{r = (res);if(r&0x80000000){DOSLogErrorMessage(WARN, "WCRM", msg, r);;DEBUG_OUT(CurrentFileLine);DEBUG_OUT(msg, ##__VA_ARGS__);DEBUG_OUT("Warning: 0x%x\n",r);/*goto Error;*/}}while(0)
 
-// Check Boolean Result
+// Check Boolean Resultre
 // Ensures that condition evaluates to true
-#define CB(condition) do{if(!(condition)) {r = R_FAIL; DOSLogErrorMessage(ERR, "CB", r);goto Error;}}while(0);
-#define WCB(condition) do{if(!(condition)) {r = R_WARNING; DOSLogErrorMessage(ERR, "WCB", r);goto Error;}}while(0);
-#define CBR(condition, failCode) do{if(!(condition)) {r = failCode; DOSLogErrorMessage(ERR, "CBR", r); goto Error;}}while(0);
-#define CBM(condition, msg, ...) do{if(!(condition)) { DOSLogErrorMessage(ERR, "CBM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); r = R_FAIL; goto Error; }}while(0);
-#define CBRM(condition, failCode, msg, ...) do{if(!(condition)) { DOSLogErrorMessage(ERR, "CBRM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); r = failCode; goto Error; }}while(0);
+#define CB(condition) do{if(!(condition)) {r = R_FAIL; DOSLogError(ERR, "CB", r);goto Error;}}while(0);
+#define WCB(condition) do{if(!(condition)) {r = R_WARNING; DOSLogError(ERR, "WCB", r);goto Error;}}while(0);
+#define CBR(condition, failCode) do{if(!(condition)) {r = failCode; DOSLogError(ERR, "CBR", r); goto Error;}}while(0);
+#define CBM(condition, msg, ...) do{if(!(condition)) { DOSLogErrorMessage(ERR, "CBM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); r = R_FAIL; goto Error; }}while(0);
+#define CBRM(condition, failCode, msg, ...) do{if(!(condition)) { DOSLogErrorMessage(ERR, "CBRM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); r = failCode; goto Error; }}while(0);
 
-#define ACBM(condition, msg, ...) do{if(!condition){DOSLogErrorMessage(ERR, "ACBM", r); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
-#define ACB(condition) do{if(!condition){DOSLogErrorMessage(ERR, "ACB", r); assert(0);}}while(0);
+#define ACBM(condition, msg, ...) do{if(!condition){DOSLogErrorMessage(ERR, "ACBM", msg, R_FAIL); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
+#define ACB(condition) do{if(!condition){DOSLogError(ERR, "ACB", R_FAIL); assert(0);}}while(0);
 
 // Check Range
-#define CRANGE(val, valMin, valMax) do{if(!(val > valMax) || !(val < valMin)) {r = R_FAIL; DOSLogErrorMessage(ERR, "CRANGE", r); goto Error;}}while(0);
+#define CRANGE(val, valMin, valMax) do{if(!(val > valMax) || !(val < valMin)) {r = R_FAIL; DOSLogError(ERR, "CRANGE", r); goto Error;}}while(0);
 
 // Check NULL Result
 // Ensures that the pointer is not a NULL
-#define CN(pointer) do{if((pointer) == NULL) {r = R_ERROR; DOSLogErrorMessage(ERR, "CN", r); goto Error;}}while(0);
-#define WCN(pointer) do{if((pointer) == NULL) {r = R_WARNING; DOSLogErrorMessage(WARN, "WCN", r); goto Error;}}while(0);
-#define CNR(pointer, failCode) do{if((pointer) == NULL) {r = failCode; DOSLogErrorMessage(ERR, "CNR", r);goto Error;}}while(0);
-#define CNM(pointer, msg, ...) do{if((pointer) == NULL) {r = R_ERROR; DOSLogErrorMessage(ERR, "CNM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
-#define WCNM(pointer, msg, ...) do{if((pointer) == NULL) { r = R_WARNING; DOSLogErrorMessage(WARN, "WCNM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n");  }}while(0);
-#define CNRM(pointer, failCode, msg, ...) do{if((pointer) == NULL) { r = failCode; DOSLogErrorMessage(ERR, "CNRM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
+#define CN(pointer) do{if((pointer) == NULL) {r = R_ERROR; DOSLogError(ERR, "CN", r); goto Error;}}while(0);
+#define WCN(pointer) do{if((pointer) == NULL) {r = R_WARNING; DOSLogError(WARN, "WCN", r); goto Error;}}while(0);
+#define CNR(pointer, failCode) do{if((pointer) == NULL) {r = failCode; DOSLogError(ERR, "CNR", r);goto Error;}}while(0);
+#define CNM(pointer, msg, ...) do{if((pointer) == NULL) {r = R_ERROR; DOSLogErrorMessage(ERR, "CNM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
+#define WCNM(pointer, msg, ...) do{if((pointer) == NULL) { r = R_WARNING; DOSLogErrorMessage(WARN, "WCNM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n");  }}while(0);
+#define CNRM(pointer, failCode, msg, ...) do{if((pointer) == NULL) { r = failCode; DOSLogErrorMessage(ERR, "CNRM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
 
-#define ACNM(pointer, msg, ...) do{if((pointer) == NULL){DOSLogErrorMessage(ERR, "ACNM", R_FAIL); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
-#define ACN(pointer) do{if((pointer) == NULL){DOSLogErrorMessage(ERR, "ACN", R_FAIL); assert(0);}}while(0);
+#define ACNM(pointer, msg, ...) do{if((pointer) == NULL){DOSLogErrorMessage(ERR, "ACNM", msg, R_FAIL); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
+#define ACN(pointer) do{if((pointer) == NULL){DOSLogError(ERR, "ACN", R_FAIL); assert(0);}}while(0);
 
 // Check is null (ensure null) result
 // Ensures that the pointer IS NULL
-#define CBN(pointer) do{if((pointer) != NULL) {r = R_ERROR; DOSLogErrorMessage(ERR, "CBM", r); goto Error;}}while(0);
-#define WCBN(pointer) do{if((pointer) != NULL) {r = R_WARNING; DOSLogErrorMessage(WARN, "WCBN", r); goto Error;}}while(0);
-#define CBNR(pointer, failCode) do{if((pointer) != NULL) {r = failCode; DOSLogErrorMessage(ERR, "CBNR", r);goto Error;}}while(0);
-#define CBNM(pointer, msg, ...) do{if((pointer) != NULL) { r = R_ERROR; DOSLogErrorMessage(ERR, "CBNM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n");  goto Error; }}while(0);
-#define WCBNM(pointer, msg, ...) do{if((pointer) != NULL) { r = R_WARNING; DOSLogErrorMessage(WARN, "CBNMW", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); }}while(0);
-#define CBNRM(pointer, failCode, msg, ...) do{if((pointer) != NULL) { r = failCode; DOSLogErrorMessage(ERR, "CBNRM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
+#define CBN(pointer) do{if((pointer) != NULL) {r = R_ERROR; DOSLogError(ERR, "CBM", r); goto Error;}}while(0);
+#define WCBN(pointer) do{if((pointer) != NULL) {r = R_WARNING; DOSLogError(WARN, "WCBN", r); goto Error;}}while(0);
+#define CBNR(pointer, failCode) do{if((pointer) != NULL) {r = failCode; DOSLogError(ERR, "CBNR", r);goto Error;}}while(0);
+#define CBNM(pointer, msg, ...) do{if((pointer) != NULL) { r = R_ERROR; DOSLogErrorMessage(ERR, "CBNM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n");  goto Error; }}while(0);
+#define WCBNM(pointer, msg, ...) do{if((pointer) != NULL) { r = R_WARNING; DOSLogErrorMessage(WARN, "CBNMW", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); }}while(0);
+#define CBNRM(pointer, failCode, msg, ...) do{if((pointer) != NULL) { r = failCode; DOSLogErrorMessage(ERR, "CBNRM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
 
-#define ABCNM(pointer, msg, ...) do{if((pointer) != NULL){DOSLogErrorMessage(ERR, "ABCNM", r); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
-#define ABCN(pointer) do{if((pointer) != NULL){DOSLogErrorMessage(ERR, "ABCN", r); assert(0);}}while(0);
+#define ABCNM(pointer, msg, ...) do{if((pointer) != NULL){DOSLogErrorMessage(ERR, "ABCNM", msg, r); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); assert(0);}}while(0);
+#define ABCN(pointer) do{if((pointer) != NULL){DOSLogError(ERR, "ABCN", r); assert(0);}}while(0);
 
 // Check Pointer Result
 // Ensures that the pointer is not a NULL
@@ -180,9 +184,9 @@ template <typename T, size_t N> char(&ArraySizeHelper(T(&array)[N]))[N];
 
 // Check Valid 
 // Ensures that the object created is valid
-#define CV(pObject) do{if(!((pObject)->IsValid())) {r = R_FAIL; DOSLogErrorMessage(ERR, "CV", r); goto Error;}}while(0);
-#define CVR(pObject, failCode) do{if(!((pObject)->IsValid())) {r = failCode; DOSLogErrorMessage(ERR, "CVR", r); goto Error;}}while(0);
-#define CVM(pObject, msg, ...) do{if(!((pObject)->IsValid())) {r = R_FAIL; DOSLogErrorMessage(ERR, "CVM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
-#define CVRM(pObject, failCode, msg, ...) do{if(!((pObject)->IsValid())) {r = failCode; DOSLogErrorMessage(ERR, "CVRM", r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
+#define CV(pObject) do{if(!((pObject)->IsValid())) {r = R_FAIL; DOSLogError(ERR, "CV", r); goto Error;}}while(0);
+#define CVR(pObject, failCode) do{if(!((pObject)->IsValid())) {r = failCode; DOSLogError(ERR, "CVR", r); goto Error;}}while(0);
+#define CVM(pObject, msg, ...) do{if(!((pObject)->IsValid())) {r = R_FAIL; DOSLogErrorMessage(ERR, "CVM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
+#define CVRM(pObject, failCode, msg, ...) do{if(!((pObject)->IsValid())) {r = failCode; DOSLogErrorMessage(ERR, "CVRM", msg, r); DEBUG_OUT(CurrentFileLine); DEBUG_OUT(msg, ##__VA_ARGS__); DEBUG_OUT("\n"); goto Error; }}while(0);
 
 #endif // ! EHM_H_
