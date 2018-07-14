1e4ce3b9c717403b21f2e39ee802d7b1dacf1022
Idan Beck <idan@dreamos.com>
2018-07-13 17:02:19 -0700

    Seems to work with proper labels, had to turn off unified since the IDs are not propagating across the connection correctly

---
+++ DreamOS/Cloud/webrtc/WebRTCAudioTrackSink.cpp
@@ -0,0 +1,21 @@
+#include "WebRTCAudioTrackSink.h"
+
+WebRTCAudioTrackSink::WebRTCAudioTrackSink(std::string strAudioTrackLabel) :
+	m_strAudioTrackLabel(strAudioTrackLabel)
+{
+	// empty
+}
+
+WebRTCAudioTrackSink::~WebRTCAudioTrackSink() {
+	// empty
+}
+
+// webrtc::AudioTrackSinkInterface
+void WebRTCAudioTrackSink::OnData(const void* pAudioBuffer, int bitsPerSample, int samplingRate, size_t channels, size_t frames) {
+
+	DEBUG_LINEOUT("OnAudioData: %s %d samples", m_strAudioTrackLabel.c_str(), (int)frames);
+
+	//if (m_pParentObserver != nullptr) {
+	//	m_pParentObserver->OnAudioData(strAudioTrackLabel, m_peerConnectionID, pAudioBuffer, bitsPerSample, samplingRate, channels, frames);
+	//}
+}
