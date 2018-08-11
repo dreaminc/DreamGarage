eace5177437b399563b1712f0afb8a7670b86641
Idan Beck <idan@dreamos.com>
2018-08-10 17:15:29 -0700

    Apparently release webrtc binaries were not actually release binaries

--- DreamOS/External/webrtc/package_webrtc.py
+++ DreamOS/External/webrtc/package_webrtc.py
@@ -1,6 +1,8 @@
+from __future__ import unicode_literals
 from os.path import join, isfile
 import shutil
 import os, shutil, sys
+
 
 # This will parse through a webrtc checkout directory and copy 
 # over the include headers as well as all of the required libraries 
@@ -8,39 +10,68 @@ import os, shutil, sys
 # usage example:
 # 	package_webrtc.py <webrtc src directory> <destination directory>
 
-# src_dir is the src folder of a webrtc checkout
-src_dir = sys.argv[1] 
+def copyFilesInTree(cpy_src_dir, cpy_dest_dir, file_ext):
+	cpy_src_dir = unicode(cpy_src_dir)
+	cpy_dest_dir = unicode(cpy_dest_dir)
+	
+	print("Copying {0} files from {1} to {2}".format(file_ext, cpy_src_dir, cpy_dest_dir))
+	
+	ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and not f.endswith(file_ext)]
+	
+	try:
+	    shutil.copytree(cpy_src_dir, cpy_dest_dir, ignore=ignore_func)	
+	except shutil.Error as err:
+	    errors.extend(err.args[0])
+	except OSError:
+		pass
 
-# dest_dir is the location to generate the package
-dest_dir = sys.argv[2]
+	
 
-errors = []
+def main():
+	# src_dir is the src folder of a webrtc checkout
+	src_dir = sys.argv[1] 
+	
+	# dest_dir is the location to generate the package
+	dest_dir = sys.argv[2]
+	
+	errors = []
+	
+	print("Packaging includes")
+	includes_src_dir = src_dir
+	includes_dest_dir = dest_dir + "/include/webrtc/"
+	copyFilesInTree(includes_src_dir, includes_dest_dir, '.h')
+	
+	print("Packaging Debug")
+	debug_src_dir = src_dir+ "/out/Debug_x64/"
+	debug_dest_dir = dest_dir + "/lib/x64/Debug/"
+	copyFilesInTree(debug_src_dir, debug_dest_dir, '.lib')
+	
+	print("Packaging Release")
+	release_src_dir = src_dir + "/out/Release_x64/"
+	release_dest_dir = dest_dir + "/lib/x64/Release/"
+	copyFilesInTree(release_src_dir , release_dest_dir, '.lib')
+	
+	print("Packaging json includes")
+	json_src_dir = src_dir + "/third_party/jsoncpp/source/include/"
+	json_dest_dir = dest_dir + "/include/jsoncpp/"
+	copyFilesInTree(json_src_dir, json_dest_dir, '.h')
+	
+	print("Packaging debug json obj")
+	json_src_dir = src_dir + "/out/Debug_x64/obj/third_party/jsoncpp/jsoncpp"
+	json_dest_dir = dest_dir + "/lib/x64/Debug/jsoncpp"
+	copyFilesInTree(json_src_dir, json_dest_dir, '.obj')
+	
+	print("Packaging release json obj")
+	json_src_dir = src_dir + "/out/Release_x64/obj/third_party/jsoncpp/jsoncpp"
+	json_dest_dir = dest_dir + "/lib/x64/Release/jsoncpp"
+	copyFilesInTree(json_src_dir, json_dest_dir, '.obj')
+	
+	# Print out errors
+	if errors:
+		raise shutil.Error(errors)
 
-print("Packaging includes")
-ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and not f.endswith('.h')]
-
-# TODO: Move into function
-try:
-	shutil.copytree(src_dir, dest_dir + "/include/webrtc/", ignore=ignore_func)	
-except shutil.Error as err:
-    errors.extend(err.args[0])
-except OSError as err:
-    errors.extend(err.args[0])
-
-print("Packaging Debug")
-ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and  not f.endswith('.lib')]
-shutil.copytree(src_dir + "/out/Debug_x64/", dest_dir + "/lib/x64/Debug/", ignore=ignore_func)
-
-print("Packaging Release")
-ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and not f.endswith('.lib')]
-shutil.copytree(src_dir + "/out/Release_x64/", dest_dir + "/lib/x64/Release/", ignore=ignore_func)
-
-print("Packaging json")
-#ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and f[-2:] != '.h']
-#copytree(src_dir + "/third_party/jsoncpp/overrides/include/", dest_dir + "/include/jsoncpp/", ignore=ignore_func)
-ignore_func = lambda d, files: [f for f in files if isfile(join(d, f)) and not f.endswith('.h')]
-shutil.copytree(src_dir + "/third_party/jsoncpp/source/include/", dest_dir + "/include/jsoncpp/", ignore=ignore_func)
-
-# Print out errors
-if errors:
-	raise shutil.Error(errors)
+	
+# execute only if run as a script
+if __name__ == "__main__":
+    
+    main()
