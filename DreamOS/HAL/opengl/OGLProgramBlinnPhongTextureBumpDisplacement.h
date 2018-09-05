6f0cfe475debb026c7428913038b831e7a4a6bb9
Idan Beck <idan@dreamos.com>
2018-09-05 15:26:25 -0700

    setting up the displacement test, and messing around with normal mapping - some of the normal mapped were flipped in Y which seemed to be causing problems

---
+++ DreamOS/HAL/opengl/OGLProgramBlinnPhongTextureBumpDisplacement.h
@@ -0,0 +1,68 @@
+#ifndef OGLPROGRAM_BLINN_PHONG_TEXTURE_BUMP_DISPLACEMENT_H_
+#define OGLPROGRAM_BLINN_PHONG_TEXTURE_BUMP_DISPLACEMENT_H_
+
+#include "./RESULT/EHM.h"
+
+// Dream OS
+// DreamOS/HAL/opengl/OGLProgramBlinnPhongTextureBumpDisplacement.h
+// Blinn Phong Texture OGL shader program - this uses the blinn phong
+// shading model along with one color texture and one bump map texture
+
+#include "OGLProgram.h"
+#include "OGLObj.h"
+#include "OGLTexture.h"
+
+class OGLProgramBlinnPhongTextureBumpDisplacement : public OGLProgram {
+public:
+	OGLProgramBlinnPhongTextureBumpDisplacement(OpenGLImp *pParentImp);
+
+	RESULT OGLInitialize();
+	virtual RESULT OGLInitialize(version versionOGL) override;
+
+	virtual RESULT SetupConnections() override;
+	virtual RESULT ProcessNode(long frameID) override;
+
+	RESULT SetObjectTextures(OGLObj *pOGLObj);
+	RESULT SetLights(std::vector<light*> *pLights);
+	RESULT SetMaterial(material *pMaterial);
+	RESULT SetObjectUniforms(DimObj *pDimObj);
+	RESULT SetCameraUniforms(camera *pCamera);
+	RESULT SetCameraUniforms(stereocamera* pStereoCamera, EYE_TYPE eye);
+
+private:
+	stereocamera *m_pCamera = nullptr;
+	ObjectStore *m_pSceneGraph = nullptr;
+
+private:
+	// Vertex Attribute
+	OGLVertexAttributePoint *m_pVertexAttributePosition;
+	OGLVertexAttributeColor *m_pVertexAttributeColor;
+	OGLVertexAttributeVector *m_pVertexAttributeNormal;
+	OGLVertexAttributeUVCoord *m_pVertexAttributeUVCoord;
+	OGLVertexAttributeVector *m_pVertexAttributeTangent;
+	OGLVertexAttributeVector *m_pVertexAttributeBitangent;
+
+	// Uniforms
+	OGLUniformMatrix4 *m_pUniformModelMatrix;
+	OGLUniformMatrix4 *m_pUniformViewMatrix;
+	//OGLUniformMatrix4 *m_pUniformProjectionMatrix;
+	OGLUniformMatrix4 *m_pUniformModelViewMatrix;
+	OGLUniformMatrix4 *m_pUniformViewProjectionMatrix;
+
+	OGLUniformPoint *m_pUniformEye;
+
+	OGLUniformSampler2D *m_pUniformTextureColor;
+	OGLUniformBool *m_pUniformHasColorTexture;
+
+	OGLUniformSampler2D *m_pUniformTextureBump;
+	OGLUniformBool *m_pUniformHasBumpTexture;
+
+	OGLUniformBool *m_pUniformHasDisplacementTexture;
+	OGLUniformSampler2D *m_pUniformTextureDisplacement;
+
+	// Uniform Blocks
+	OGLLightsBlock *m_pLightsBlock;
+	OGLMaterialBlock *m_pMaterialsBlock;
+};
+
+#endif // ! OGLPROGRAM_BLINN_PHONG_TEXTURE_BUMP_DISPLACEMENT_H_
