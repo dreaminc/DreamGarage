eee62de57e489540031cb3138327b9220e206db5
Idan Beck <idan@dreamos.com>
2018-09-17 18:36:56 -0700

    pr comments

--- DreamOS/Sense/SenseGamePadController.cpp
+++ DreamOS/Sense/SenseGamePadController.cpp
@@ -1,7 +1,6 @@
 #include "SenseGamepadController.h"
 
-SenseGamepadController::SenseGamepadController()
-{
+SenseGamepadController::SenseGamepadController() {
 	memset(&m_currentGamepadState, 0, sizeof(SenseGamepadState));
 
 	for (int i = 0; i < SENSE_GAMEPAD_INVALID; i++) {
@@ -19,17 +18,20 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 	if (gpState.leftJoystick != m_currentGamepadState.leftJoystick) {
 		NotifySubscribers(SENSE_GAMEPAD_JOYSTICK_LEFT, &SenseGamepadEvent(SENSE_GAMEPAD_JOYSTICK_LEFT, gpState.leftJoystick));
 	}
+	
 	if (gpState.rightJoystick != m_currentGamepadState.rightJoystick) {
 		NotifySubscribers(SENSE_GAMEPAD_JOYSTICK_RIGHT, &SenseGamepadEvent(SENSE_GAMEPAD_JOYSTICK_RIGHT, gpState.rightJoystick));
 	}
+	
 	if (gpState.leftTriggerRange != m_currentGamepadState.leftTriggerRange) {
 		point2D triggerAsPoint;
-		triggerAsPoint(0, 0) = gpState.leftTriggerRange;
+		triggerAsPoint.x() = gpState.leftTriggerRange;
 		NotifySubscribers(SENSE_GAMEPAD_TRIGGER_LEFT, &SenseGamepadEvent(SENSE_GAMEPAD_TRIGGER_LEFT, triggerAsPoint));
 	}
+
 	if (gpState.rightTriggerRange != m_currentGamepadState.rightTriggerRange) {
 		point2D triggerAsPoint;
-		triggerAsPoint(0, 0) = gpState.rightTriggerRange;
+		triggerAsPoint.x() = gpState.rightTriggerRange;
 		NotifySubscribers(SENSE_GAMEPAD_TRIGGER_RIGHT, &SenseGamepadEvent(SENSE_GAMEPAD_TRIGGER_RIGHT, triggerAsPoint));
 	}
 
@@ -41,6 +43,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_DPAD_UP));
 		}
 	}
+	
 	if (gpState.buttonStruct.fDpadDown != m_currentGamepadState.buttonStruct.fDpadDown) {
 		if (gpState.buttonStruct.fDpadDown) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_DPAD_DOWN));
@@ -49,6 +52,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_DPAD_DOWN));
 		}
 	}
+	
 	if (gpState.buttonStruct.fDpadLeft != m_currentGamepadState.buttonStruct.fDpadLeft) {
 		if (gpState.buttonStruct.fDpadLeft) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_DPAD_LEFT));
@@ -57,6 +61,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_DPAD_LEFT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fDpadRight != m_currentGamepadState.buttonStruct.fDpadRight) {
 		if (gpState.buttonStruct.fDpadRight) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_DPAD_RIGHT));
@@ -65,6 +70,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_DPAD_RIGHT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fStart != m_currentGamepadState.buttonStruct.fStart) {
 		if (gpState.buttonStruct.fStart) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_START));
@@ -73,6 +79,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_START));
 		}
 	}
+	
 	if (gpState.buttonStruct.fSelect != m_currentGamepadState.buttonStruct.fSelect) {
 		if (gpState.buttonStruct.fSelect) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_SELECT));
@@ -81,6 +88,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_SELECT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fLeftThumb != m_currentGamepadState.buttonStruct.fLeftThumb) {
 		if (gpState.buttonStruct.fLeftThumb) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_BUTTON_JOYSTICK_LEFT));
@@ -89,6 +97,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_BUTTON_JOYSTICK_LEFT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fRightThumb != m_currentGamepadState.buttonStruct.fRightThumb) {
 		if (gpState.buttonStruct.fRightThumb) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_BUTTON_JOYSTICK_RIGHT));
@@ -97,6 +106,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_BUTTON_JOYSTICK_RIGHT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fLeftShoulder != m_currentGamepadState.buttonStruct.fLeftShoulder) {
 		if (gpState.buttonStruct.fLeftShoulder) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_SHOULDER_LEFT));
@@ -105,6 +115,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_SHOULDER_LEFT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fRightShoulder != m_currentGamepadState.buttonStruct.fRightShoulder) {
 		if (gpState.buttonStruct.fRightShoulder) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_SHOULDER_RIGHT));
@@ -113,6 +124,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_SHOULDER_RIGHT));
 		}
 	}
+	
 	if (gpState.buttonStruct.fbuttonA != m_currentGamepadState.buttonStruct.fbuttonA) {
 		if (gpState.buttonStruct.fbuttonA) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_A));
@@ -121,6 +133,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_A));
 		}
 	}
+	
 	if (gpState.buttonStruct.fbuttonB != m_currentGamepadState.buttonStruct.fbuttonB) {
 		if (gpState.buttonStruct.fbuttonB) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_B));
@@ -129,6 +142,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_B));
 		}
 	}
+	
 	if (gpState.buttonStruct.fbuttonX != m_currentGamepadState.buttonStruct.fbuttonX) {
 		if (gpState.buttonStruct.fbuttonX) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_X));
@@ -137,6 +151,7 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_UP, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_UP, SENSE_GAMEPAD_X));
 		}
 	}
+	
 	if (gpState.buttonStruct.fbuttonY != m_currentGamepadState.buttonStruct.fbuttonY) {
 		if (gpState.buttonStruct.fbuttonY) {
 			NotifySubscribers(SENSE_GAMEPAD_BUTTON_DOWN, &SenseGamepadEvent(SENSE_GAMEPAD_BUTTON_DOWN, SENSE_GAMEPAD_Y));
@@ -148,6 +163,6 @@ RESULT SenseGamepadController::SetGamepadState(SenseGamepadState gpState) {
 
 	m_currentGamepadState = gpState;
 
-//Error:
+Error:
 	return r;
 }
