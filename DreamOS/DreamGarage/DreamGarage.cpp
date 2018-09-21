5c9035d1df9830001f146cb48ab4ff4973916911
jaisrael <jason.a.israel@gmail.com>
2018-09-21 13:27:23 -0700

    cleanup

--- DreamOS/DreamGarage/DreamGarage.cpp
+++ DreamOS/DreamGarage/DreamGarage.cpp
@@ -629,7 +629,7 @@ RESULT DreamGarage::DidFinishLoading() {
 	CN(m_pUserController);
 	
 	// DEBUG:
-//#ifdef _DEBUG
+#ifdef _DEBUG
 	{
 		m_fHasCredentials = true;
 
@@ -658,7 +658,7 @@ RESULT DreamGarage::DidFinishLoading() {
 			return m_pUserController->GetAccessToken(strDebugRefreshToken);
 		}
 	}
-//#endif
+#endif
 	
 	// Initial step of login flow:
 	if(IsConnectedToInternet()) {
