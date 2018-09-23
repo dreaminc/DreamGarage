2d6bc2554abbe471658e25735c63346e64e452c5
Idan Beck <idan@dreamos.com>
2018-09-22 18:28:34 -0700

    clean up

--- DreamOS/DreamGarage/DreamGarage.cpp
+++ DreamOS/DreamGarage/DreamGarage.cpp
@@ -637,7 +637,7 @@ RESULT DreamGarage::DidFinishLoading() {
 	CN(m_pUserController);
 	
 	// DEBUG:
-//#ifdef _DEBUG
+#ifdef _DEBUG
 	{
 		m_fHasCredentials = true;
 
@@ -666,7 +666,7 @@ RESULT DreamGarage::DidFinishLoading() {
 			return m_pUserController->GetAccessToken(strDebugRefreshToken);
 		}
 	}
-//#endif
+#endif
 	
 	// Initial step of login flow:
 	if(IsConnectedToInternet()) {
