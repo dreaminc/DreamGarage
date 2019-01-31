2f3f7a59d783b28c99b330586cce67e9373b1d64
jaisrael <jason.a.israel@gmail.com>
2019-01-31 15:11:57 -0800

    initial shader program work pre billboard primitive revision

---
+++ DreamOS/HAL/opengl/OGLProgramBillboard.cpp
@@ -0,0 +1,113 @@
+#include "OGLProgramBillboard.h"
+
+#include "Scene/ObjectStoreImp.h"
+#include "Scene/ObjectStore.h"
+
+#include "Primitives/stereocamera.h"
+
+#include "HAL/opengl/OpenGLImp.h"
+#include "HAL/opengl/OGLFramebuffer.h"
+
+#include "OGLAttachment.h"
+
+OGLProgramBillboard::OGLProgramBillboard(OpenGLImp *pParentImp, PIPELINE_FLAGS optFlags = PIPELINE_FLAGS::NONE) :
+	OGLProgramMinimal(pParentImp, optFlags)
+{
+	// empty
+}
+
+RESULT OGLProgramBillboard::OGLInitialize() {
+	RESULT r = R_PASS;
+
+	CR(OGLProgram::OGLInitialize());
+
+
+	CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformModelMatrix), std::string("u_mat4Model")));
+	CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformViewProjectionMatrix), std::string("u_mat4ViewProjection")));
+	//CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformViewMatrix), std::string("u_mat4View")));
+	//CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformProjectionMatrix), std::string("u_mat4Projection")));
+
+	CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformViewWidth), std::string("u_width")));
+	CR(RegisterUniform(reinterpret_cast<OGLUniform**>(&m_pUniformViewHeight), std::string("u_height")));
+
+	// Framebuffer Output
+	if (IsPassthru() == false) {
+		int pxWidth = m_pParentImp->GetViewport().Width();
+		int pxHeight = m_pParentImp->GetViewport().Height();
+
+		m_pOGLFramebuffer = new OGLFramebuffer(m_pParentImp, pxWidth, pxHeight, 4);
+		CR(m_pOGLFramebuffer->OGLInitialize());
+		CR(m_pOGLFramebuffer->Bind());
+
+		CR(m_pOGLFramebuffer->SetSampleCount(1));
+
+		CR(m_pOGLFramebuffer->MakeColorAttachment());
+		CR(m_pOGLFramebuffer->GetColorAttachment()->MakeOGLTexture(texture::type::TEXTURE_2D));
+		CR(m_pOGLFramebuffer->GetColorAttachment()->AttachTextureToFramebuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0));
+
+		CR(m_pOGLFramebuffer->MakeDepthAttachment());
+		CR(m_pOGLFramebuffer->GetDepthAttachment()->OGLInitializeRenderBuffer());
+
+		CR(m_pOGLFramebuffer->InitializeOGLDrawBuffers(1));
+	}
+
+Error:
+	return r;
+}
+
+RESULT OGLProgramBillboard::OGLInitialize(version versionOGL) {
+	RESULT r = R_PASS;
+
+	CR(OGLInitialize());
+
+	m_versionOGL = versionOGL;
+
+	// Global
+	CRM(AddSharedShaderFilename(L"core440.shader"), "Failed to add global shared shader code");
+	CRM(AddSharedShaderFilename(L"materialCommon.shader"), "Failed to add shared vertex shader code");
+
+	// Vertex
+	CRM(MakeVertexShader(L"billboard.vert"), "Failed to create vertex shader");
+
+	// Geometry
+	CRM(MakeGeometryShader(L"billboard.geo"), "Failed to create geometry shader");
+
+	// Fragment
+	CRM(MakeFragmentShader(L"billboard.frag"), "Failed to create vertex shader");
+
+	CRM(LinkProgram(), "Failed to link program");
+
+	WCR(GetVertexAttributesFromProgram());
+	WCR(BindAttributes());
+
+	CR(GetUniformVariablesFromProgram());
+
+	CR(GetUniformBlocksFromProgram());
+	CR(BindUniformBlocks());
+
+	// TODO:  Currently using a global material 
+	SetMaterial(&material(60.0f, 1.0f, color(COLOR_WHITE), color(COLOR_WHITE), color(COLOR_WHITE)));
+
+Error:
+	return r;
+}
+
+RESULT OGLProgramBillboard::ProcessNode(long frameID) {
+	return R_PASS;
+}
+
+RESULT OGLProgramBillboard::SetupConnections() {
+	return R_PASS;
+}
+
+RESULT OGLProgramBillboard::SetCameraUniforms(camera *pCamera) {
+	return R_PASS;
+}
+
+RESULT OGLProgramBillboard::SetCameraUniforms(stereocamera* pStereoCamera, EYE_TYPE eye) {
+	return R_PASS;
+}
+
+RESULT OGLProgramBillboard::SetObjectUniforms(DimObj *pDimObj) {
+	return R_PASS;
+}
---
+++ DreamOS/HAL/opengl/OGLProgramBillboard.h
@@ -0,0 +1,34 @@
+#ifndef OGLPROGRAM_BILLBOARD_H_
+#define OGLPROGRAM_BILLBOARD_H_
+
+#include "./RESULT/EHM.h"
+#include "OGLProgramMinimal.h"
+
+class ObjectStore;
+class stereocamera;
+
+class OGLProgramBillboard : public OGLProgramMinimal {
+public:
+	OGLProgramBillboard(OpenGLImp *pParentImp, PIPELINE_FLAGS optFlags = PIPELINE_FLAGS::NONE);
+
+	virtual RESULT ProcessNode(long frameID) override;
+
+	virtual RESULT OGLInitialize() override;
+	virtual RESULT OGLInitialize(version versionOGL) override;
+
+	virtual RESULT SetupConnections() override;
+
+	virtual RESULT SetCameraUniforms(camera *pCamera) override;
+	virtual RESULT SetCameraUniforms(stereocamera* pStereoCamera, EYE_TYPE eye) override;
+	virtual RESULT SetObjectUniforms(DimObj *pDimObj) override;
+
+protected:
+	OGLUniformMatrix4 *m_pUniformProjectionMatrix = nullptr;
+	OGLUniformMatrix4 *m_pUniformViewMatrix = nullptr;
+
+	OGLUniform *m_pUniformViewWidth;
+	OGLUniform *m_pUniformViewHeight;
+};
+
+
+#endif // ! OGLPROGRAM_BILLBOARD_H_
