45814c0ff63baba841edba695498221ed13a5785
John Lee <31673314+JohnL33@users.noreply.github.com>
2019-02-06 22:02:36 -0800

    it all connects

--- DreamOS/DreamGarage/DreamGarage.cpp
+++ DreamOS/DreamGarage/DreamGarage.cpp
@@ -1326,6 +1326,9 @@ RESULT DreamGarage::OnAudioData(const std::string &strAudioTrackLabel, PeerConne
 		AudioPacket pendingPacket((int)frames, (int)channels, (int)bitsPerSample, (int)samplingRate, (uint8_t*)pAudioDataBuffer);
 		CR(m_pDreamSoundSystem->PlayAudioPacketSigned16Bit(pendingPacket, strAudioTrackLabel, channel));
 
+		// hack to make them connect?
+		pendingPacket.SetSoundType(sound::type::SIGNED_16_BIT);
+		
 		// Send audio to Mixdown
 		CR(PushAudioPacketToMixdown((int)frames, pendingPacket));
 
@@ -1348,6 +1351,10 @@ RESULT DreamGarage::OnAudioData(const std::string &strAudioTrackLabel, PeerConne
 
 			AudioPacket pendingPacket((int)frames, (int)channels, (int)bitsPerSample, (int)samplingRate, (uint8_t*)pAudioDataBuffer);
 			CR(m_pDreamSoundSystem->PlayAudioPacketSigned16Bit(pendingPacket, strAudioTrackLabel, channel));
+
+			// hack to make them connect?
+			// same as with peer mic
+			pendingPacket.SetSoundType(sound::type::SIGNED_16_BIT);
 
 			// Send audio to Mixdown
 			CR(PushAudioPacketToMixdown((int)frames, pendingPacket));
