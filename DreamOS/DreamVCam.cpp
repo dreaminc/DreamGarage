dd7da20be510f76ae06b1b49b0f11124769c932a
jaisrael <jason.a.israel@gmail.com>
2019-02-08 19:57:03 -0800

    pr cleanup

--- DreamOS/DreamVCam.cpp
+++ DreamOS/DreamVCam.cpp
@@ -222,7 +222,7 @@ RESULT DreamVCam::Update(void *pContext) {
 
 		size_t bufferSize = m_pStreamingTexture->GetTextureSize();
 		m_loadBufferIndex = (m_loadBufferIndex + 1) % 2;
-		m_pStreamingTexture->LoadFlippedBufferFromTexture(m_pLoadBuffer[m_loadBufferIndex], bufferSize);
+		m_pStreamingTexture->LoadBufferFromTexture(m_pLoadBuffer[m_loadBufferIndex], bufferSize);
 	}
 	/*
 	else if (!m_fIsRunning) {
@@ -234,14 +234,13 @@ RESULT DreamVCam::Update(void *pContext) {
 	}
 	//*/
 
-	//CBR(m_fIsRunning, R_SKIPPED);
+	CBR(m_fIsRunning, R_SKIPPED);
 
 	CNR(m_pOGLEndNode, R_SKIPPED);
 	CNR(m_pOGLRenderNode, R_SKIPPED);
 	//*
 	// TODO: Some more logic around texture / buffer sizes etc 
 	//if (m_pNamedPipeServer != nullptr && m_pSourceTexture != nullptr) {
-	if (m_fIsRunning)
 	{
 		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
 
@@ -307,7 +306,7 @@ RESULT DreamVCam::Update(void *pContext) {
 					m_pStreamingTexture = m_pClosedTexture;
 					size_t bufferSize = m_pStreamingTexture->GetTextureSize();
 					m_loadBufferIndex = (m_loadBufferIndex + 1) % 2;
-					m_pStreamingTexture->LoadFlippedBufferFromTexture(m_pLoadBuffer[m_loadBufferIndex], bufferSize);
+					m_pStreamingTexture->LoadBufferFromTexture(m_pLoadBuffer[m_loadBufferIndex], bufferSize);
 
 					if (std::chrono::duration_cast<std::chrono::milliseconds>(timeNow - m_msTimeClosed).count() > 1000) {
 						m_fPendDisconnectPipes = false;
@@ -546,14 +545,9 @@ RESULT DreamVCam::CloseSource() {
 
 	m_fPendDisconnectPipes = true;
 	m_msTimeClosed = std::chrono::system_clock::now();
-//	m_fIsRunning = false;
 
 	m_pParentApp->HandleCameraClosed();
 	SetSourceType(SourceType::CAMERA);
-
-	//m_pStreamingTexture = m_pClosedTexture;
-	//m_fIsClosed = true;
-	//SetSourceType(SourceType::CLOSED);
 
 Error:
 	return r;
