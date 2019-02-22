81df9b26935f49cffde9457a8d54fea769d83b64
Idan Beck <idanbeck@gmail.com>
2019-02-22 14:15:31 -0800

    initial test project - confirming test

---
+++ DreamOS/Project/Windows/DreamUnitTesting/DreamOSTests.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "RESULT/EHM.h"
+
+#include "./../DreamTestApp/DreamTestApp.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DreamUnitTesting {		
+	TEST_CLASS(DreamOSTests) {
+	public:
+		
+		TEST_METHOD(VaniallaDOSTest) {
+			RESULT r = R_PASS;
+
+			// Mock argc/argv 
+			
+			const char *argv[] = { "DreamTestApp.exe", "-ts", "webrtc", "-t", "0" };
+			int argc = sizeof(argv) / sizeof(argv[0]);
+
+			DEBUG_LINEOUT("DUT:DOS: Vanilla Test");
+
+			CRM(r, "Vanialla check RESULT code test");
+
+			//DreamTestApp dreamTestApp;
+			//CRM(dreamTestApp.Initialize(argc, argv), "Failed to initialize Dream Test App");
+			//CRM(dreamTestApp.Start(), "Failed to start Dream Test App");	// This is the entry point for the DreamOS Engine
+
+		Error:
+			Assert::AreEqual(r, R_PASS);
+		}
+
+	};
+}
