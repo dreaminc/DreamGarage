f0b2c9ad12868b6f7cdec6b267360268ebc66250
Idan Beck <idanbeck@gmail.com>
2019-09-18 16:34:00 -0700

    initial test code working with glfw and vulkan

---
+++ src/junk/vulkan/VulkanApp.h
@@ -0,0 +1,27 @@
+#ifndef VULKAN_JUNK_APP_
+#define VULKAN_JUNK_APP_
+
+#include "core/ehm/EHM.h"
+
+// Dream Vulkan Junk App
+// dos/src/junk/vulkan/VulkanApp.h
+
+
+class VulkanApp {
+
+public:
+	VulkanApp() {
+
+	}
+
+	~VulkanApp() {
+
+	}
+
+	static RESULT TestVulkanSetup();
+
+//private:
+
+};
+
+#endif // ! VULKAN_JUNK_APP_
